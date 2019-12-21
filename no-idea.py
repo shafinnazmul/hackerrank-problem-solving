1608ecea6c2e49ba1e1d851a6d457734c8431aee
Nazmul Hoque shafin <shafin.nazmul@gmail.com>
2019-12-21 21:58:52 +0600

    problem:no-ideas solved

--- no-idea.py
+++ no-idea.py
@@ -1,13 +1,6 @@
 if __name__=='__main__':
-    n,m=(map(int,input().split()))
-    set_x = list(map(int, input().split()))
-    set_a = list(map(int, input().split()))
-    set_b = list(map(int, input().split()))
-    set_x_a = (i  for i in set_x if i in set_a)
-    set_x_b = (i for i in set_x if i in set_b)
-    hp,up=0,0
-    for i in set_x_a:
-        hp+=1
-    for i in set_x_b:
-        up+=1
-    print(hp- up)
+    n,m=input().split()
+    set_x = input().split()
+    set_a = set(input().split())
+    set_b = set(input().split())
+    print(sum([(i in set_a) - (i in set_b) for i in set_x]))
